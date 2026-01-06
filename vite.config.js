33019d126ba74f60b615ff27c23fa01ab318b2fa
Junaid Ekram <112276644+junaidekram@users.noreply.github.com>
2026-01-06 14:35:16 +0000

    updated for render deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   server: {
     port: 8000,
     open: "?local",
